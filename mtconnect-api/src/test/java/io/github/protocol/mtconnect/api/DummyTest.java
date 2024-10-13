7e20202866a3c2b2a264569bd7dc356c7b68e90c
ZhangJian He <shoothzj@gmail.com>
2024-10-13 19:32:09 +0800

    build: add dummy test in api module (#10)
    Signed-off-by: ZhangJian He <shoothzj@gmail.com>

---
+++ mtconnect-api/src/test/java/io/github/protocol/mtconnect/api/DummyTest.java
@@ -0,0 +1,12 @@
+package io.github.protocol.mtconnect.api;
+
+import org.junit.jupiter.api.Test;
+
+public class DummyTest {
+    /**
+     * test method to trigger the api module statistics
+     */
+    @Test
+    public void dummy() {
+    }
+}
